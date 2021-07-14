56c8bfc28358a94fb5dee5289ba85eb806526844
Diana Ramírez <dcramirezm_2@uqvirtual.edu.co>
2021-07-13 20:43:37 -0500

    Eliminado

--- src/lexico/Error.kt
+++
@@ -1,8 +0,0 @@
-package lexico
-
-class Error(var fila: Int, var columna: Int, var mensaje: String) {
-
-    override fun toString(): String {
-        return "Error [fila=$fila, columna=$columna, mensaje=$mensaje]"
-    }
-}
